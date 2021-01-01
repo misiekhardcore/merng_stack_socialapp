a4c2a4d1189408652276ce505ff2f8ca9935b6f2
misiekhardcore <misiekhardcore@gmail.com>
2021-01-01 22:17:13 +0100

    add likePost and subscription to newPost
    With pubsub context you can now subscribe to get information whenever
    new post appears. likePost let to like/unlike post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,7 @@
+const {
+  UserInputError,
+  AuthenticationError,
+} = require("apollo-server");
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
 
@@ -26,7 +30,7 @@ module.exports = {
     },
   },
   Mutation: {
-    async createPost(_, { body }, context) {
+    createPost: async (_, { body }, context) => {
       const user = checkAuth(context);
 
       const newPost = new Post({
@@ -38,9 +42,13 @@ module.exports = {
 
       const post = await newPost.save();
 
+      context.pubsub.publish("NEW_POST", {
+        newPost: post,
+      });
+
       return post;
     },
-    async deletePost(_, { postId }, context) {
+    deletePost: async (_, { postId }, context) => {
       const user = checkAuth(context);
 
       try {
@@ -59,6 +67,35 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //Post already liked, unlike it
+          post.likes = post.likes.filter(
+            (like) => like.username !== username
+          );
+        } else {
+          //Post not liked, like it
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) =>
+        pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
