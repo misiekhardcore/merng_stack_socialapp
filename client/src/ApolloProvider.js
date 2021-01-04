8368fa865d5710631106f559fc5b767f886e4482
misiekhardcore <misiekhardcore@gmail.com>
2021-01-04 21:55:11 +0100

    add authorization header
    When user is logged in, add token to authorization header

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,13 +6,23 @@ import {
   createHttpLink,
   ApolloProvider,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httplink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httplink,
+  link: authLink.concat(httplink),
   cache: new InMemoryCache(),
 });
 
