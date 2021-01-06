cbd4cb50d719c397534f7c2667800553b17122fc
misiekhardcore <misiekhardcore@gmail.com>
2021-01-06 12:58:20 +0100

    bugfix and popups

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Icon, Label } from "semantic-ui-react";
+import { Link, useHistory } from "react-router-dom";
+import { Button, Icon, Label, Popup } from "semantic-ui-react";
 
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 
 const LikeButton = ({ user, post: { likesCount, likes, id } }) => {
   const [liked, setLiked] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
     if (
@@ -26,21 +27,38 @@ const LikeButton = ({ user, post: { likesCount, likes, id } }) => {
     },
   });
 
+  const handleLike = () => {
+    if (user) likePost();
+    else history.push("/login");
+  };
+
   return (
-    <Button as="div" labelPosition="right" onClick={likePost}>
-      <Button color="red" basic={liked ? false : true}>
-        <Icon name="heart" />
-      </Button>
-      {user ? (
-        <Label as="a" basic color="red" pointing="left">
-          {likesCount}
-        </Label>
-      ) : (
-        <Label as={Link} to="/login" basic color="red" pointing="left">
-          {likesCount}
-        </Label>
-      )}
-    </Button>
+    <Popup
+    inverted
+      content={`${liked?"Unlike":"Like"} post`}
+      trigger={
+        <Button as="div" labelPosition="right" onClick={handleLike}>
+          <Button color="red" basic={liked ? false : true}>
+            <Icon name="heart" />
+          </Button>
+          {user ? (
+            <Label as="a" basic color="red" pointing="left">
+              {likesCount}
+            </Label>
+          ) : (
+            <Label
+              as={Link}
+              to="/login"
+              basic
+              color="red"
+              pointing="left"
+            >
+              {likesCount}
+            </Label>
+          )}
+        </Button>
+      }
+    />
   );
 };
 
