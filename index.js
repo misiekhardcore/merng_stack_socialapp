a91f124ffe93ec8c61ec5297353ebe1bf1429345
misiekhardcore <misiekhardcore@gmail.com>
2021-01-07 12:29:18 +0100

    bugfix/clear

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    return server.listen({ port: process.env.port || 5000 });
+    return server.listen({ port: process.env.PORT || 5000 });
   })
   .then((res) => {
     console.log(`server running at ${res.url}`);
